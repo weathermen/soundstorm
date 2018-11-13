cc551112c1ae6be7c49e5c4df72969d89c7c54f2
Tom Scott <tscott@weblinc.com>
2018-11-12 20:48:49 -0500

    Work on job stuff

--- app/jobs/broadcast_message_job.rb
+++ app/jobs/broadcast_message_job.rb
@@ -3,8 +3,8 @@ class BroadcastMessageJob < ApplicationJob
   queue_as :default
 
   def perform(version)
-    version.followers.each do |follower|
-      ActivityPub.deliver(version.message, to: follower.host)
+    version.followers.all? do |follower|
+      ActivityPub.deliver(version.message, to: follower.host).success?
     end
   end
 end
--- test/jobs/broadcast_message_job_test.rb
+++ test/jobs/broadcast_message_job_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BroadcastMessageJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'broadcast message when changes occur' do
+    skip
+
+    version = versions(:one)
+    message = version.message
+    host = version.whodunnit.followers.first.host
+
+    ActivityPub.stub :deliver, message, to: host do
+      assert BroadcastMessageJob.perform_now(version)
+    end
+  end
 end
