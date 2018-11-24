d59f0b6702131fef84efc96666de1a87aa59cd7f
Tom Scott <tscott@weblinc.com>
2018-11-24 02:28:12 -0500

    Convert hardcoded text to translations

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -3,6 +3,15 @@ module TracksHelper
     bindings = { target: 'player.button', action: 'click->player#toggle' }
 
     return unless audio.attached?
-    button_to 'Play', audio.url, data: bindings
+    button_to t('.play'), audio.url, data: bindings
+  end
+
+  def player_for(track, &block)
+    data = {
+      controller: 'player',
+      track: url_for([track.user, track])
+    }
+
+    content_tag :section, class: 'player', data: data, &block
   end
 end
