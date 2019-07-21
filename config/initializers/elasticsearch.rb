1debdea72d622f543c1899b3e066e9616f96cc00
Tom Scott <tscott@weblinc.com>
2019-07-21 14:32:51 -0400

    Fix ES URL

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 # Point to Elasticsearch server in Docker network
-Elasticsearch::Model.client = Elasticsearch::Client.new(url: 'search:9200')
+Elasticsearch::Model.client = Elasticsearch::Client.new(
+  url: ENV.fetch('ELASTICSEARCH_URL', 'search:9200')
+)
